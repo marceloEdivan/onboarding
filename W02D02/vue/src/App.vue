8447b9a5cf3e9577f7eeb4b4c48966f8cbd8f41d
Marcelo <marcelo.edivan@prevision.com.br>
2022-04-25 08:20:27 -0300

    W02D02

---
+++ W02D02/vue/src/App.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+v-app
+  div(class="container")
+    h1 Cadastro de Ferramentas
+    v-divider
+    ListTools
+</template>
+
+<script>
+import ListTools from './components/ListTools.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ListTools
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ W02D02/vue/src/components/ListTools.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+div(class="container")
+	AddTool(v-on:add-tool="addTool")
+	p(v-if="tools.length <= 0") Nenhuma ferramenta cadastrada...
+	
+	div(class="list-group")
+		CardTool(v-for="(tool, index) in allTools", :key="index", v-bind:tool="tool", v-on:remove-tool='removeTool')
+		
+</template>
+
+<script>
+import AddTool from './AddTool.vue'
+import CardTool from './CardTool.vue'
+
+
+export default {
+	components: {
+		AddTool,
+		CardTool
+	},
+
+	data () {
+		return {
+			tools : []
+		}
+	},
+
+	methods: {
+		addTool(tool) {
+			this.tools.push(tool)
+		},
+
+		removeTool(index) {
+			this.tools.splice(index, 1)
+		}
+	},
+
+	computed: {
+		allTools() {
+			return this.tools.map(tool => ({
+				...tool,
+				icon: !tool.type.trim() ? 'mdi-help-circle-outline' : (tool.type === 'Manual' ? 'mdi-pipe-wrench' : 'mdi-power-plug'),
+				type: !tool.type.trim() ? 'Não especificado' : tool.type,
+			}))
+		}
+	},
+
+	watch: {
+		tools(val){
+			console.log('Ferramentas alteradas')
+			console.log(val)
+		}
+	}
+}
+</script>
+
+<style>
+</style>
